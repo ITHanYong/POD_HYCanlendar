e88fc0abc5bf9a150cdfb87adc1b2f7d24477e5f
HanYong <1152829171@qq.com>
2020-06-29 15:48:03 +0800

    version 1.0.0

---
+++ POD_HYCanlendar/Classes/ReplaceMe.m
@@ -0,0 +1,11 @@
+/**
+ cocoapods 公共库更新版本操作
+ git add .
+ git commit -m "version 1.0.1"
+ git push origin master
+
+ git tag '1.0.1'
+ git push --tags
+ pod trunk push xxx.podspec --allow-warnings
+ 
+ */

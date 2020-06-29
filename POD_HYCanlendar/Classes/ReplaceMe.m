227489f19dd4f2d8bb3d8aff27c8332ff16e1af0
HanYong <1152829171@qq.com>
2020-06-29 16:32:20 +0800

    version1.0.3

--- POD_HYCanlendar/Classes/ReplaceMe.m
+++ POD_HYCanlendar/Classes/ReplaceMe.m
@@ -1,19 +1,21 @@
 /**
  cocoapods 公共库更新版本操作
- 1、将更新代码提交到git上并更新tag值
- git add .
- git commit -m "version 1.0.2"
- git push origin master
+ 
+ 1、修改POD_HYCanlendar.podspec文件如下内容
+    s.version          = '1.0.2'
+    s.source           = { :git => 'https://github.com/ITHanYong/POD_HYCanlendar.git', :tag => "1.0.2" }
+ 注意：将子代码版本同步改为1.0.2
+ 
+ 2、将更新代码提交到git上并更新tag值
+    git add .
+    git commit -m "version 1.0.2"
+    git push origin master
 
  git tag '1.0.2'
  git push --tags
  
- 2、修改POD_HYCanlendar.podspec文件如下内容
- s.version          = '1.0.2'
- s.source           = { :git => 'https://github.com/ITHanYong/POD_HYCanlendar.git', :tag => "1.0.2" }
- 
  3、执行如下命令验证并提交到cocoapods
- pod lib lint POD_HYCanlendar.podspec --allow-warnings
- pod trunk push POD_HYCanlendar.podspec --allow-warnings
+    pod lib lint POD_HYCanlendar.podspec --allow-warnings
+    pod trunk push POD_HYCanlendar.podspec --allow-warnings
  
  */
